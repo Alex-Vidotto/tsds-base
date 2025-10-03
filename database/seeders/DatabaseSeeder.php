08d7c3d60ee23ae59ec9f5e05e77da932646cb93
Alex-Vidotto <Vidottoalexe@gmail.com>
2025-10-03 12:29:55 -0300

    Cambios pequeños en usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RoleSeeder::class);        
         $this->call(UserSeeder::class);
+        
         $this->call(CarBrandSeeder::class);
         $this->call(CarModelSeeder::class);
         $this->call(CarSeeder::class);
         $this->call(CarServiceSeeder::class);
         $this->call(CarServiceDateSeeder::class);
         $this->call(TareaSeeder::class);
-        $this->call(RoleSeeder::class);
     }
 }
