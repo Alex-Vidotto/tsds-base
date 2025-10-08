0c3927876897b85eb3b55b4e4508e140016c20ba
Hernan Rodriguez <hernanrodriguez.bk@gmail.com>
2025-10-08 17:51:22 -0300

    modal

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,4 @@ Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])->name('tar
 Route::get('/tareas{tarea}', [TareaController::class, 'show'])->name('tareas.show')->middleware('auth');
 Route::resource('cars', App\Http\Controllers\CarController::class)->middleware('auth'); // Sigue el estándar de Laravel
 Route::resource('carservicedates', App\Http\Controllers\CarServiceDateController::class)->middleware('auth');
+});
