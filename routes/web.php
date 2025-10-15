2b390f2d85c36a5697e75376825f2f12edd86618
Richard-bit120 <arzarichard@gmail.com>
2025-10-15 18:02:54 -0300

    App\Http\Controllers\TareaController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TareaController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\CarServiceDateController;
